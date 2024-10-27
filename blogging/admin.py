7913f10032766a7b1a4884ca06cf2592bb21950b
bryan-UW <145146099+bryan-UW@users.noreply.github.com>
2024-10-27 14:18:21 -0700

    Completed Assingment 7 (I Think)

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInlineAdmin(admin.TabularInline):
+    model = Category.posts.through  
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInlineAdmin] 
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)  
+
+
+# admin.site.register(Post)
+# admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
